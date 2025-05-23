1f8f43b6196f63684217067cd9a7eba68cdf71db
Vasiliy Konstantinov <nikerra90@gmail.com>
2025-05-23 22:53:29 +0300

    Create encodeDecode.go

---
+++ encodeDecode.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type UseAll struct {
+	Name    string `json:"username"`
+	Surname string `json:"surname"`
+	Year    int    `json:"created"`
+}
+
+func main() {
+	useall := UseAll{Name: "Vasiliy", Surname: "Konstantinov", Year: 2021}
+
+	t, err := json.Marshal(&useall)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Printf("Value %s\n", t)
+	}
+
+	str := `{"username": "V.", "surname": "Ko", "created":2020}`
+	jsonRecord := []byte(str)
+	temp := UseAll{}
+	err = json.Unmarshal(jsonRecord, &temp)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Printf("Data type: %T with value %v\n", temp, temp)
+	}
+}
